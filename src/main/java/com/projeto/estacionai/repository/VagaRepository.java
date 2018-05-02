b274054f9c149089e6e383581a6a17338242af90
Bluthgen <ra99164@uem.br>
2018-05-02 14:20:54 -0300

    Serviços das Vagas

---
+++ src/main/java/com/projeto/estacionai/repository/VagaRepository.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projeto.estacionai.repository;
+
+import com.projeto.estacionai.model.Bloco;
+import com.projeto.estacionai.model.Vaga;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author User
+ */
+@Repository
+public interface VagaRepository extends JpaRepository<Vaga, Long> {
+    public List<Vaga> findByBlocoLike(Bloco bloco);
+
+}
---
+++ src/main/java/com/projeto/estacionai/service/VagaService.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projeto.estacionai.service;
+
+import com.projeto.estacionai.model.Vaga;
+import com.projeto.estacionai.repository.VagaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Eduardo
+ */
+@Service
+public class VagaService {
+    @Autowired
+    private VagaRepository repository;
+	
+    public void salvar(Vaga vaga)
+    {
+        this.repository.save(vaga);
+    }
+	
+    public void deletar(Long id)
+    {
+        this.repository.deleteById(id);
+    }
+	
+    public List<Vaga> buscarTodos()
+    {
+        return this.repository.findAll();
+    }
+	
+    public Vaga buscar(Long id)
+    {
+        return this.repository.getOne(id);
+    }	
+}
+
