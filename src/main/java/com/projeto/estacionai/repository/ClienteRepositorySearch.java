5ea5f9ea25621542d30f89758e0f82f2ad134e79
guigzp <guizamberlam@hotmail.com>
2018-05-02 19:37:25 -0300

    Adicionando funcionalidades CRUD Cliente

---
+++ src/main/java/com/projeto/estacionai/repository/ClienteRepositorySearch.java
@@ -0,0 +1,67 @@
+package com.projeto.estacionai.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import com.projeto.estacionai.model.Cliente;
+
+@Repository
+public class ClienteRepositorySearch {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	
+	public List<Cliente> filtrar(Cliente cliente) {
+		CriteriaBuilder builder =  manager.getCriteriaBuilder();
+		CriteriaQuery<Cliente> criteria = builder.createQuery(Cliente.class);
+		
+		Root<Cliente> root = criteria.from(Cliente.class);
+		
+		Predicate[] predicates = restricoes(cliente, builder, root);
+		criteria.where(predicates);
+		
+		TypedQuery<Cliente> query = manager.createQuery(criteria);
+		
+		return query.getResultList(); 
+	}
+	
+	private Predicate[] restricoes(Cliente cliente, CriteriaBuilder builder, Root<Cliente> root) {
+		List<Predicate> predicates	= new ArrayList<>();
+		
+		
+		if(!StringUtils.isEmpty(cliente.getCpf()))
+			predicates.add(builder.equal(builder.lower(root.get("cpf")), 
+				cliente.getCpf().toLowerCase() ));
+		if(!StringUtils.isEmpty(cliente.getEndereco()))
+			predicates.add(builder.like(builder.lower(root.get("endereco")), 
+					"%"+ cliente.getEndereco().toLowerCase()+ "%"));
+		if(!StringUtils.isEmpty(cliente.getNome()))
+			predicates.add(builder.equal(builder.lower(root.get("nome")), 
+					"%" + cliente.getNome().toLowerCase() + "%"));
+		if(!StringUtils.isEmpty(cliente.getTelefone()))
+			predicates.add(builder.equal(builder.lower(root.get("telefone")), 
+					"%" + cliente.getTelefone().toLowerCase() + "%"));
+		if(cliente.getNumeroVagas() != null)
+			predicates.add(builder.equal(root.get("numeroVagas"), 
+					cliente.getNumeroVagas()));
+		if(cliente.getTipoPagamento() != null)
+			predicates.add(builder.equal(root.get("tipoPagamento"), 
+					cliente.getTipoPagamento()));
+		
+		
+		return predicates.toArray(new Predicate[predicates.size()]);
+	}
+	
+}
