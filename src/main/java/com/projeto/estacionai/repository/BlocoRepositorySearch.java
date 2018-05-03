fb89e8caef3e0834d80499b55f06f6d93f4bbffe
Bluthgen <ra99164@uem.br>
2018-05-02 22:13:44 -0300

    Controller

---
+++ src/main/java/com/projeto/estacionai/repository/BlocoRepositorySearch.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projeto.estacionai.repository;
+
+import com.projeto.estacionai.model.Bloco;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Eduardo
+ */
+@Repository
+public class BlocoRepositorySearch {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	
+	public List<Bloco> filtrar(Bloco bloco) {
+		CriteriaBuilder builder =  manager.getCriteriaBuilder();
+		CriteriaQuery<Bloco> criteria = builder.createQuery(Bloco.class);
+		
+		Root<Bloco> root = criteria.from(Bloco.class);
+		
+		Predicate[] predicates = restricoes(bloco, builder, root);
+		criteria.where(predicates);
+		
+		TypedQuery<Bloco> query = manager.createQuery(criteria);
+		
+		return query.getResultList(); 
+	}
+	
+	private Predicate[] restricoes(Bloco bloco, CriteriaBuilder builder, Root<Bloco> root) {
+		List<Predicate> predicates	= new ArrayList<>();
+		
+		
+		if(bloco.getNumVagas() != null)
+			predicates.add(builder.equal(root.get("numVagas"), 
+					bloco.getMaxVagas()));
+		if(bloco.getMaxVagas() != null)
+			predicates.add(builder.equal(root.get("maxVagas"), 
+					bloco.getMaxVagas()));
+		
+		
+		return predicates.toArray(new Predicate[predicates.size()]);
+	}
+	
+}
+
